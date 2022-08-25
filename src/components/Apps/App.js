53acf642e793b58d6ae36c8d9305c395969b94d2
Mona Lisa <stefanibusiness@gmail.com>
2022-08-24 20:10:30 -0700

    initial commit

---
+++ src/components/Apps/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+
+const Apps = () => 
+
+<div className='apps'>
+    <div className='app'>
+        <img src="https://picsum.photos/200/200" alt='app1' />
+        <h2>App1</h2>
+        <h4>Description: this is my description of the app</h4>
+    </div>
+
+    <div className='app'>
+        <img src="https://picsum.photos/200/200" alt='app1' />
+        <h2>App1</h2>
+        <h4>Description: this is my description of the app</h4>
+    </div>
+
+    <div className='app'>
+        <img src="https://picsum.photos/200/200" alt='app1' />
+        <h2>App1</h2>
+        <h4>Description: this is my description of the app</h4>
+    </div>
+
+    <div className='app'>
+        <img src="https://picsum.photos/200/200" alt='app1' />
+        <h2>App1</h2>
+        <h4>Description: this is my description of the app</h4>
+    </div>
+
+    <div className='app'>
+        <img src="https://picsum.photos/200/200" alt='app1' />
+        <h2>App1</h2>
+        <h4>Description: this is my description of the app</h4>
+    </div>
+
+</div>
+
+export default Apps;
