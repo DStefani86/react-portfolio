a403139b453b25a4053d01d1654da12f97905f5c
Mona Lisa <stefanibusiness@gmail.com>
2022-08-25 20:51:58 -0700

    totally broken

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import Nav from "./components/Nav";
 import Work from "./components/Work";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-const App = () => {
-  const [getPage, setPage] = useState("about");
-
-  const showPage = page => 
-    page === 'about' 
-      ? <About /> 
-      : page === 'work' 
-      ? <Work /> 
-      : page === 'contact'
-      ? <Contact />
-      : '';
-
+function App () {
   return (
-    <>
-      <Nav currentPage={setPage} />
-      <main>
-        {showPage(getPage)}
-      </main>
-    </>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home}/>
+        <Route path="/about" exact component={About}/>
+        <Route path="/work" exact component={Work}/>
+        <Route path="/contact" exact component={Contact}/>
+      </div>
+    </Router>
+  
   );
 };
+
+const Home = () => (
+  <div>
+    <h1>Welcome to my site!</h1>
+  </div>
+);
 
 export default App;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,25 @@
-import './Nav.css';
+import React from "react";
+import "../../index.css";
+import { Link } from 'react-router-dom';
 
-const Nav = props => 
+
+function Nav() {
+    return (
     <nav>
         <h1>Daniel Stefani</h1>
         <div>
+            <Link to='/about'>
             <button>About Me</button>
-            <button>Portfolio</button>
+            </Link>
+            <Link to='/work'>
+            <button>Work</button>
+            </Link>
+            <Link to='/contact'>
             <button>Contact</button>
+            </Link>
             <button>Resume</button>
         </div>
     </nav>
-
-
+    );
+}
 export default Nav;
